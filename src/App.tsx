c4b7052a48788d5a56a8e59e6f6a94f43e7323b6
isabellediasr <isabellediasr1@gmail.com>
2025-03-13 23:12:50 -0300

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 function App() {
-   return ( 
-   <div>
-      
-   </div>
+   return (
+      <div className="w-screen h-screen bg-[#1c1a1d]">
+         <div className="flex justify-center items-center">
+            <header>
+               <h1 className="font-jetbrains text-white">
+                  Dev<span>Task</span>
+               </h1>
+            </header>
+            <section className="w-[50%] bg-[#2b2e33]">
+               <input type="text" placeholder="Digite o título da tarefa" />
+               <input type="text" placeholder="Digite a descrição da tarefa" />
+            </section>
+         </div>
+      </div>
    );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const router = createBrowserRouter([
     {
